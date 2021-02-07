d5998253ca27ad4f32002396c49b382ff757e0f7
Raphael Correa <raphaelcorreaesilva@gmail.com>
2021-02-06 21:42:55 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## Messaging
+
+A simple application for messaging
+
+##### Build
+
+<pre>
+mvn clean package
+</pre>
+
+##### Run unit tests
+
+<pre>
+mvn test
+</pre>
+
+##### Run application
+
+<pre>
+java -jar target/Messaging-1.0-SNAPSHOT.jar
+</pre>
---
+++ src/test/java/messaging/MessagingAppTest.java
@@ -0,0 +1,15 @@
+package messaging;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MessagingAppTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
