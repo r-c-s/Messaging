b82b5199c7bdb6234323fa03f2ed23cc895b1173
Raphael Correa <raphaelcorreaesilva@gmail.com>
2021-02-07 19:06:31 -0300

    add first endpoint, disable security (for now)

---
+++ src/main/java/messaging/controller/MessagingController.java
@@ -0,0 +1,19 @@
+package messaging.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MessagingController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> helloWorld() {
+        return ResponseEntity.ok()
+                .contentType(MediaType.TEXT_PLAIN)
+                .body("Hello world!");
+    }
+}
