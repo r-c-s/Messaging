6a6e6fb877fadbb6e015e0a14da44276e2bb719a
Raphael Correa <raphaelcorreaesilva@gmail.com>
2021-02-11 15:29:47 -0300

    endpoint to send messages

--- src/main/java/rv/messaging/controllers/MessagingController.java
+++ src/main/java/rv/messaging/controllers/MessagingController.java
@@ -1,19 +1,32 @@
 package rv.messaging.controllers;
 
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import rv.messaging.models.MessageSendRequest;
+import rv.messaging.services.MessageService;
+
+import java.time.LocalDateTime;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/messages")
 public class MessagingController {
 
-    @GetMapping("/hello")
-    public ResponseEntity<String> helloWorld() {
-        return ResponseEntity.ok()
-                .contentType(MediaType.TEXT_PLAIN)
-                .body("Hello world!");
+    private MessageService messageService;
+
+    public MessagingController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> sendMessage(@RequestBody MessageSendRequest request) {
+        messageService.sendMessage(
+                "user", // need to figure out use based on credentials"
+                request,
+                LocalDateTime.now());
+
+        return ResponseEntity.ok().build();
     }
 }
