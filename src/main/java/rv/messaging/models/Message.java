e1789b99a48134ae079db257b0e1b0a0d16b652d
Raphael Correa <raphaelcorreaesilva@gmail.com>
2021-02-11 11:01:16 -0300

    add @Document annotation

--- src/main/java/rv/messaging/models/Message.java
+++ src/main/java/rv/messaging/models/Message.java
@@ -1,9 +1,11 @@
 package rv.messaging.models;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
+@Document(collection = "messages")
 public class Message {
 
     @Id
